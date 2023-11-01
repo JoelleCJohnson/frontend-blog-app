6d459ed79f3dee05a4f84f55ed132168e40c66b7
Joelle  Johnson <joellej221@gmail.com>
2023-11-01 11:15:26 -0400

    working on front end

---
+++ src/components/Nav.jsx
@@ -0,0 +1,23 @@
+export default function Nav() {
+    return (
+        <header>
+            <ul>
+                <li>
+                    <a href="/" target="_blank" rel="noopener noreferrer">
+                        Home
+                    </a>
+                </li>
+                <li>
+                    <a href="http://" target="_blank" rel="noopener noreferrer">
+                        Add Post
+                    </a>
+                </li>
+                <li>
+                    <a href="http://" target="_blank" rel="noopener noreferrer">
+                        Log in
+                    </a>
+                </li>
+            </ul>
+        </header>
+    )
+} 
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import Nav from "../components/Nav"
+import Footer from "../components/Footer"
+import BlogList from "../components/BlogList"
+import BlogCard from "../components/BlogCard"
+
+export default function Home() {
+    return (
+        <>
+            <Nav />
+            Home page
+            <Footer />
+        </>
+    )
+}
